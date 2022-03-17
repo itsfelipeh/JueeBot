98c3e0f55eee8f601e4a47ed7f8738c087fa8a4e
itsfelipeh <itsfelipeh@itsfelipeh.com>
2022-03-17 14:39:03 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ def read_token():
     with open("token.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
-tokenc= read_token()
+token= read_token()
 
 client = discord.Client()
 
